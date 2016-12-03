d0719b00dd11ed1a22fec5c37dcf15c892a4b8d3
Marian Dolinský <mariandolinsky@outlook.com>
2016-12-03 07:19:07 +0100

    Added UWPHelper from https://github.com/bramborman/UWPHelper

---
+++ UWPHelper/Converters/BoolToBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace UWPHelper.Converters
+{
+    // Use for binding bool? to bool or vice versa
+    public sealed class BoolToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return System.Convert.ToBoolean(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return System.Convert.ToBoolean(value);
+        }
+    }
+}
---
+++ UWPHelper/Utilities/Delayer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Windows.UI.Xaml;
+
+namespace UWPHelper.Utilities
+{
+    // Inspired by Rudy Huyn - http://www.rudyhuyn.com/blog/2016/03/01/delay-an-action-debounce-and-throttle/
+    public sealed class Delayer
+    {
+        private DispatcherTimer timer;
+
+        public bool IsRunning
+        {
+            get { return timer.IsEnabled; }
+        }
+
+        public event EventHandler Tick;
+
+        public Delayer(double secondsInterval) : this(TimeSpan.FromSeconds(secondsInterval))
+        {
+
+        }
+
+        public Delayer(TimeSpan interval)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = interval;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            timer.Stop();
+            Tick?.Invoke(this, new EventArgs());
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+
+        public void Reset()
+        {
+            timer.Stop();
+            timer.Start();
+        }
+    }
+}
